ca0cc83a9644eba0a75bec3466c0ae82d4dcaf4e
FrostSource <jjhfrost@gmail.com>
2022-08-24 15:53:15 +1200

    Portal fix
    Player must have head on correct side first before teleporting happens.

--- scripts/vscripts/bivouac/ancient_gate.lua
+++ scripts/vscripts/bivouac/ancient_gate.lua
@@ -4,15 +4,25 @@ require"util.player"
 local UPDATE_INTERVAL = 0
 
 local is_exit = thisEntity:GetName() == "portal_exit" and true or false
+local player_must_cross_boundary_first = false
+
+---Get if a position is across the portal boundary.
+---@param pos Vector
+---@return boolean
+local function IsAcrossBoundary(pos)
+    return (is_exit and pos.x < 0) or (not is_exit and pos.x > 0)
+end
 
 local function Thinker()
     -- needs to be refined to avoid slow peeks
     local loc = thisEntity:TransformPointWorldToEntity(Player:EyePosition())
-    if (is_exit and loc.x < 0) or (not is_exit and loc.x > 0) then
+    if IsAcrossBoundary(loc) and not player_must_cross_boundary_first then
         -- DoEntFire(thisEntity:GetName().."_tp", "Enable", "", 0, nil, nil)
         thisEntity:FireOutput("OnUser1", thisEntity, thisEntity, nil, 0)
         Player:EmitSound("ScriptedSeq.InnerVaultBubbleExpand")
         return nil
+    elseif player_must_cross_boundary_first then
+        player_must_cross_boundary_first = false
     end
 
     if (is_exit and loc.x >= 0) or (not is_exit and loc.x <= 0) then
@@ -25,6 +35,7 @@ local function Thinker()
 end
 
 local function EnableThink()
+    player_must_cross_boundary_first = true
     thisEntity:SetThink(Thinker, "Thinker", 0)
 end
 thisEntity:GetPrivateScriptScope().EnableThink = EnableThink
